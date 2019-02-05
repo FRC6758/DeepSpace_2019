00fb8778b01f9a506ed73e3e4693dea4fb86c868
Computer #1 <6758@devjoe.net>
2019-02-05 16:51:53 -0600

    DriveTrain starter

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -7,7 +7,12 @@
 
 package frc.robot.subsystems;
 
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.commands.Drive;
+import frc.robot.extra.BrushlessMotor;
 
 /**
  * Add your docs here.
@@ -15,10 +20,19 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class DriveTrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+  private static BrushlessMotor FL = new BrushlessMotor(3);
+  private static BrushlessMotor FR = new BrushlessMotor(0);
+  private static BrushlessMotor BL = new BrushlessMotor(2);
+  private static BrushlessMotor BR = new BrushlessMotor(1);
+
+  private static SpeedControllerGroup leftMotor = new SpeedControllerGroup(FL, BL);
+  private static SpeedControllerGroup rightMotor = new SpeedControllerGroup(FR, BR);
+
+  public static DifferentialDrive drive = new DifferentialDrive(leftMotor, rightMotor);
 
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+     setDefaultCommand(new Drive());
   }
 }
